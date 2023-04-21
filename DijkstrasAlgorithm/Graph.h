a0fed51ce1d3c6264163fb97dcc9500122b5e32c
John Mo <peikaimo@gmail.com>
2023-04-21 00:14:03 -0500

    Temp Project 3 Work

--- DijkstrasAlgorithm/Graph.h
+++ DijkstrasAlgorithm/Graph.h
@@ -6,27 +6,27 @@
 #include <vector>
 using std::vector;
 
-
 class Edge;
 
 class Vertex
 {
 public:
     int label;
-    vector<Edge*> edges;
+    vector<Edge *> edges;
 
     // helpers stored inside a vertex to help with path finding
     // you can use different auxilliary structures if desired
     bool visited;
     float distanceTo;
-    vector<Vertex*> pathTo;
-    Heap<Vertex*>::Locator locator;
-    bool operator < ( const Vertex &v ) const 
-    { 
+    vector<Vertex *> pathTo;
+    Heap<Vertex *>::Locator locator;
+    bool operator<(const Vertex &v) const
+    {
         // TODO for locator heap implementation
+        return distanceTo < v.distanceTo;
     }
 
-    Vertex ( int l ) : label (l) { }
+    Vertex(int l) : label(l) {}
 };
 
 class Edge
@@ -35,39 +35,89 @@ public:
     Vertex *v1, *v2;
     float weight;
 
-    Edge ( Vertex *nv1, Vertex *nv2, float newWeight ) : v1 ( nv1 ), v2 ( nv2 ), weight ( newWeight ) { }
+    Edge(Vertex *nv1, Vertex *nv2, float newWeight) : v1(nv1), v2(nv2), weight(newWeight) {}
 };
-
 
 class Graph
 {
 protected:
-    vector<Vertex*> v;
-    vector<Edge*> e;
+    vector<Vertex *> v;
+    vector<Edge *> e;
 
 public:
-    Graph () {}
+    Graph() {}
 
-    ~Graph ()
+    ~Graph()
     {
         // TODO
+        for (Vertex *vertex : v)
+        {
+            delete vertex;
+        }
+        for (Edge *edge : e)
+        {
+            delete edge;
+        }
     }
 
     // TODO: complete rule of 3
 
-    void insertVertex ( int label )
+    void insertVertex(int label)
     {
         // TODO
+        v.push_back(new Vertex(label));
     }
 
-    void insertEdge ( int l1, int l2, float weight )
+    void insertEdge(int l1, int l2, float weight)
     {
         // TODO
+        Vertex *v1 = v[l1];
+        Vertex *v2 = v[l2];
+        Edge *edge = new Edge(v1, v2, weight);
+        v1->edges.push_back(edge);
+        v2->edges.push_back(edge);
+        e.push_back(edge);
     }
 
-    vector<Vertex*> shortestPath ( int start, int end )
+    vector<Vertex *> shortestPath(int start, int end)
     {
         // TODO: Dijkstra's algorithm
+        Heap<Vertex *> heap;
+        v[start]->distanceTo = 0;
+        v[start]->locator = heap.insertElement(v[start]);
+
+        while (!heap.isEmpty())
+        {
+            Vertex *current = heap.removeMin();
+            current->visited = true;
+            if (current->label == end)
+                break;
+
+            for (Edge *edge : current->edges)
+            {
+                Vertex *neighbor = (edge->v1 == current) ? edge->v2 : edge->v1;
+                if (neighbor->visited)
+                    continue;
+
+                float newDistance = current->distanceTo + edge->weight;
+                if (newDistance < neighbor->distanceTo)
+                {
+                    neighbor->distanceTo = newDistance;
+                    neighbor->pathTo = current->pathTo;
+                    neighbor->pathTo.push_back(current);
+
+                    if (neighbor->locator)
+                    {
+                        heap.update(neighbor->locator);
+                    }
+                    else
+                    {
+                        neighbor->locator = heap.insertElement(neighbor);
+                    }
+                }
+            }
+        }
+        return v[end]->pathTo;
     }
 };
 
--- DijkstrasAlgorithm/main.cpp
+++ DijkstrasAlgorithm/main.cpp
@@ -7,27 +7,53 @@ using namespace std;
 
 #include "Graph.h"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
     cout << "Filename of graph to load: ";
 
     // TODO: next five code snippets
     // Snippet 1: read filename and open file
+    string filename;
+    cin >> filename;
+
+    ifstream inputFile(filename);
+
+    if (!inputFile)
+    {
+        cout << "Error opening file: " << filename << endl;
+        return 1;
+    }
 
     // Snippet 2: get number graph size
-    
+    int numVertices, numEdges;
+    inputFile >> numVertices, numEdges;
+
     // Snippet 3: create graph
+    Graph graph;
+    for (int i = 0; i < numVertices; i++)
+        graph.insertVertex(i);
 
     // Snippet 4: read edges
+    for (int i = 0; i < numEdges; i++)
+    {
+        int l1, l2;
+        float weight;
+        inputFile >> l1 >> l2 >> weight;
+        graph.insertEdge(l1, l2, weight);
+    }
 
     // Snippet 5: read start and end of path
-    
+    int startVertex, endVertex;
+    inputFile >> startVertex >> endVertex;
+
     cout << "Shortest path from " << startVertex << " to " << endVertex << ":" << endl;
 
-    vector<Vertex*> path;
+    vector<Vertex *> path;
 
     // TODO: call shortest path on your graph for the sstart and end verices and save result to path
-    
-    for (auto i : path) { // this is a for-each loop
+    path = graph.shortestPath(startVertex, endVertex);
+    for (auto i : path)
+    { // this is a for-each loop
         cout << i->label << " ";
     }
     // cout endline at very end
